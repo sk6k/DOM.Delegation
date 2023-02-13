ae3ce27ad3723a4c7af948b429e87e258c31a480
Andrii <reyandrey98@gmail.com>
2023-02-13 17:50:19 +0200

    update

--- delegation.js
+++ delegation.js
@@ -1,91 +1,77 @@
-const instruments = [
-	{
-		id: 1,
-		img: "https://static.dnipro-m.ua/cache/products/1754/catalog_origin_141546.jpg",
-		name: "Молоток",
-		price: 150,
-	},
-	{
-		id: 2,
-		img: "https://static.dnipro-m.ua/cache/products/5098/catalog_origin_195568.jpg",
-		name: "Перфоратор",
-		price: 3000,
-	},
-	{
-		id: 3,
-		img: "https://static.dnipro-m.ua/cache/products/2023/catalog_origin_200763.jpg",
-		name: "Рівень",
-		price: 2000,
-	},
-];
+// const instruments = [
+// 	{
+// 		id: 1,
+// 		img: "https://static.dnipro-m.ua/cache/products/1754/catalog_origin_141546.jpg",
+// 		name: "Молоток",
+// 		price: 150,
+// 	},
+// 	{
+// 		id: 2,
+// 		img: "https://static.dnipro-m.ua/cache/products/5098/catalog_origin_195568.jpg",
+// 		name: "Перфоратор",
+// 		price: 3000,
+// 	},
+// 	{
+// 		id: 3,
+// 		img: "https://static.dnipro-m.ua/cache/products/2023/catalog_origin_200763.jpg",
+// 		name: "Рівень",
+// 		price: 2000,
+// 	},
+// ];
 
-const list = document.querySelector(".list");
-// const markup = instruments
-// 	.map(
-// 		({ id, img, name, price }) =>
-// `<li data-id="${id}">
-// <img src="${img}" alt="${name}" width="300">
-//     <h2>${name}</h2>
-// 	<h3>${price}</h3></li>`
-// 	)
-// 	.join("");
+// const list = document.querySelector(".list");
 
-const markup = instruments.reduce(
-	(acc, { id, img, name, price }) =>
-		acc +
-		`<li data-product-id="${id}" class="js-item">
-            <img src="${img}" alt="${name}" width="150">
-                <h2>${name}</h2>
-				<h3>Ціна:${price}</h3>
-				<button type="button" class="js-cart">Add to cart</button>
-				<button type="button" class="js-favorite">Add to favorite</button></li>
-				`,
-	""
-);
+// const markup = instruments.reduce(
+// 	(acc, { id, img, name, price }) =>
+// 		acc +
+// 		`<li data-product-id="${id}" class="js-item">
+//             <img src="${img}" alt="${name}" width="150">
+//                 <h2>${name}</h2>
+// 				<h3>Ціна:${price}</h3>
+// 				<button type="button" class="js-cart">Add to cart</button>
+// 				<button type="button" class="js-favorite">Add to favorite</button></li>
+// 				`,
+// 	""
+// );
 
-list.insertAdjacentHTML("beforeend", markup);
+// list.insertAdjacentHTML("beforeend", markup);
 
-// const li = document.querySelector("li");
-// li.setAttribute("data-discount", 15);
-// console.log(li.getAttribute("data-discount"));
-// console.log(li.hasAttribute("data-discount"));
+// const basket = [];
+// const favorite = [];
 
-const basket = [];
-const favorite = [];
+// list.addEventListener("click", onClick);
 
-list.addEventListener("click", onClick);
+// function onClick(event) {
+// 	if (event.target.nodeName !== "BUTTON") {
+// 		return;
+// 	}
+// 	if (event.target.classList.contains("js-cart")) {
+// 		const product = { ...getElement(event.target) };
+// 		basket.push(product);
+// 		console.log(basket);
+// 	} else if (event.target.classList.contains("js-favorite")) {
+// 		const product = { ...getElement(event.target) };
+// 		const haveProduct = favorite.find(({ id }) => id === product.id);
 
-function onClick(event) {
-	if (event.target.nodeName !== "BUTTON") {
-		return;
-	}
-	if (event.target.classList.contains("js-cart")) {
-		const product = { ...getElement(event.target) };
-		basket.push(product);
-		console.log(basket);
-	} else if (event.target.classList.contains("js-favorite")) {
-		const product = { ...getElement(event.target) };
-		const haveProduct = favorite.find(({ id }) => id === product.id);
+// 		if (haveProduct) {
+// 			haveProduct.qty += 1;
+// 		} else {
+// 			product.qty = 1;
+// 			favorite.push(product);
+// 		}
 
-		if (haveProduct) {
-			haveProduct.qty += 1;
-		} else {
-			product.qty = 1;
-			favorite.push(product);
-		}
+// 		console.log("favorite:", favorite);
+// 	}
+// }
 
-		console.log("favorite:", favorite);
-	}
-}
-
-/**
- * Find and return current product
- * @param {Object} elem
- * @returns {Object}
- */
-function getElement(elem) {
-	const currentElement = elem.closest(".js-item");
-	const productId = Number(currentElement.dataset.productId);
-	const product = instruments.find(({ id }) => id === productId);
-	return product;
-}
+// /**
+//  * Find and return current product
+//  * @param {Object} elem
+//  * @returns {Object}
+//  */
+// function getElement(elem) {
+// 	const currentElement = elem.closest(".js-item");
+// 	const productId = Number(currentElement.dataset.productId);
+// 	const product = instruments.find(({ id }) => id === productId);
+// 	return product;
+// }
---
+++ throttle_debounce.js
@@ -0,0 +1,66 @@
+// const cats = [
+// 	"https://i.ytimg.com/vi/iKA6ZXpGcGQ/maxresdefault.jpg",
+// 	"https://i.natgeofe.com/n/46b07b5e-1264-42e1-ae4b-8a021226e2d0/domestic-cat_thumb_square.jpg",
+// 	"https://cdn.britannica.com/39/7139-050-A88818BB/Himalayan-chocolate-point.jpg",
+// 	"https://i.guim.co.uk/img/media/941093798b256e5d1aa6246a08824bc86f62a31f/0_0_5000_3000/master/5000.jpg?width=1300&quality=85&fit=max&s=ea672fb16df8634901283252059baaf8",
+// 	"https://nypost.com/wp-content/uploads/sites/2/2022/06/reddit-cats-judging-looks-00.jpg?quality=75&strip=all",
+// 	"https://gdb.voanews.com/09690000-0a00-0242-5408-08da76ebde16_cx0_cy3_cw99_w1200_r1.jpg",
+// 	"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRCK8yYjQ6igjrAJyCyXUwV6U7RDkLVc4FVnQ&usqp=CAU",
+// 	"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRWEQxVHz2ZzUdIKINgA-LvmuZgoady4hu4hQ&usqp=CAU",
+// 	"https://e3.365dm.com/22/07/768x432/skynews-cats-pet_5846440.jpg?20220726201324",
+// 	"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT4xXPTgr5Lug8qU_akXp2oC_4m5w3FoAvx_Q&usqp=CAU",
+// 	"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTnB4if5HpwDvEqkgBShHrRvbwYCya8vzB86g&usqp=CAU",
+// 	"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRWEQxVHz2ZzUdIKINgA-LvmuZgoady4hu4hQ&usqp=CAU",
+// 	"https://e3.365dm.com/22/07/768x432/skynews-cats-pet_5846440.jpg?20220726201324",
+// 	"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT4xXPTgr5Lug8qU_akXp2oC_4m5w3FoAvx_Q&usqp=CAU",
+// 	"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTnB4if5HpwDvEqkgBShHrRvbwYCya8vzB86g&usqp=CAU",
+// 	"https://assets.globalwildlife.org/m/3b6039a74fd067ea/webimage-Tigrinia.jpg",
+// 	"https://pbs.twimg.com/profile_images/378800000532546226/dbe5f0727b69487016ffd67a6689e75a_400x400.jpeg",
+// 	"https://www.nhm.ac.uk/content/dam/nhmwww/discover/wild-cats/margay-two-column.jpg.thumb.768.768.jpg",
+// 	"https://irs.www.warnerbros.com/gallery-v2-jpeg/cats__dogs_photo_12-14571309.jpg",
+// 	"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRIyGk9Ds3bdd4B404EcoMDrVUKnU5u9KABOg&usqp=CAU",
+// ];
+
+// const list = document.querySelector(".js-list");
+
+// const markup = cats.map(
+// 	(item) => `<li>
+// 				<img src="${item}" alt="cat" loading="lazy" width="200">
+//                 			<p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Omnis velit numquam natus quibusdam cupiditate libero unde aliquid dignissimos, aspernatur quaerat dolorem dolore praesentium culpa saepe! Sit soluta error dicta ut!
+// 			Maxime quod quibusdam voluptatum a veniam perspiciatis eos maiores soluta cum? Assumenda repellendus, architecto laboriosam veritatis ipsum, facere animi vel adipisci voluptatum quaerat quisquam qui voluptas, voluptates id mollitia nesciunt?
+// 			Necessitatibus, consequuntur inventore ducimus reprehenderit, dolore sapiente similique officia corporis, iste animi dolorem ipsam soluta quae debitis quam autem quod? Repudiandae suscipit exercitationem ad! Provident facilis ad accusantium possimus modi.
+// 			Sunt, cum illo fugiat illum magnam eum repellendus reiciendis iste accusantium debitis sint aliquid, ducimus itaque cumque, porro odit at! Saepe, non itaque! Sequi expedita et amet laudantium eligendi dignissimos?
+// 			Inventore neque, est rerum, quaerat voluptatum quod officiis quae quos voluptatibus reprehenderit officia pariatur quia assumenda optio explicabo perferendis, impedit consequuntur sint voluptatem. Expedita sunt temporibus ab repellat, beatae earum!
+// 			Explicabo, eligendi et eum magnam sed tempore libero quos omnis voluptate dolor sunt voluptatibus similique aliquid voluptas! Quae omnis, ad voluptate delectus beatae sed magni saepe ullam voluptatem facilis quo!
+// 			Ducimus ipsum, hic amet repudiandae a optio eos repellat suscipit laborum explicabo ad alias quam sapiente vitae et. Error obcaecati et exercitationem alias tempora odit maxime accusamus dolor ex delectus.
+// 			Aperiam ducimus enim delectus adipisci, culpa numquam, sit iusto quas dolorum libero harum incidunt officia quibusdam repellendus temporibus ipsum corporis totam dolore earum est fugit. Quam perspiciatis voluptatem blanditiis molestias!
+// 			Adipisci quibusdam doloribus facere beatae magnam aut, numquam perspiciatis, animi, hic nam doloremque quos eveniet maiores odio iste! Quisquam vel ducimus eaque, quos asperiores voluptas quo. Rerum sed officia ratione!
+// 			Labore libero ipsam, accusamus perspiciatis eligendi rem. Qui deleniti quo similique. Quisquam, inventore? Fugit tempore perferendis dicta incidunt recusandae doloremque tempora quaerat ullam adipisci molestias, corrupti, reiciendis minima error reprehenderit?
+// 			Molestiae vel quod earum voluptas, repellat accusantium animi consequatur similique possimus rerum obcaecati esse? Et, dignissimos! At reiciendis deserunt quasi enim eligendi minima ut eius cupiditate, quae similique asperiores ipsum!
+// 			Fugit ratione molestias laudantium nisi incidunt rerum aliquid aspernatur quae! Quam officiis autem, dolor officia architecto est accusamus et voluptatum maxime, totam necessitatibus nulla blanditiis corporis, deleniti inventore alias nobis!
+// 			Iusto amet inventore, sit dicta necessitatibus optio fugiat nam dolor, corrupti ab voluptatum fugit expedita natus aperiam provident enim recusandae? Placeat omnis adipisci numquam, totam nisi dicta. Atque, facilis eligendi!
+// 			Magni, magnam? Reprehenderit, saepe excepturi ipsam fugit voluptatem sit labore qui obcaecati. Quas pariatur totam non tempore nam et repudiandae saepe neque tempora consequuntur. Sapiente porro alias perspiciatis odit sunt?
+// 			Beatae ipsam vel est. At vel voluptatem libero. Dolorum eius, incidunt repellat praesentium aliquam hic. Odio quis asperiores dicta est laboriosam, aliquid at! Blanditiis dolores mollitia unde ab, explicabo recusandae?
+// 			Repellat non, totam et minima, explicabo perspiciatis adipisci provident hic molestias facilis aspernatur cumque quasi fuga? Architecto molestias aliquam quam numquam praesentium, aliquid blanditiis. Natus cum facere expedita ea ut.
+// 			Ratione itaque beatae accusantium rerum, eius ipsum harum saepe non vitae nemo! Optio similique, fugiat, rerum tempore hic a nihil assumenda quas facere officiis cum aut mollitia, molestias labore! Ipsam.
+// 			Qui praesentium veniam nesciunt ipsa repellendus sint nulla nobis, asperiores reiciendis iure eos, ex odit cum dolorum quis laudantium architecto voluptates porro numquam magni est quod eius laboriosam suscipit. Doloremque.
+// 			Pariatur nostrum eos cum sed totam sit architecto consequuntur. At accusamus ipsam eius odio ex exercitationem provident ad voluptates! Nesciunt nostrum id quis molestias doloremque omnis beatae pariatur cum quos?
+// 			Atque at quaerat debitis voluptate iusto temporibus ab, voluptas quae sunt expedita ea! Eius molestias corporis, optio, assumenda aliquam aperiam alias praesentium harum aut, doloribus suscipit exercitationem laborum eligendi. Iure?</p>
+// 			</li>`
+// );
+
+// list.insertAdjacentHTML("beforeend", markup);
+
+// document.addEventListener("scroll", _.throttle(onScroll, 500));
+
+// function onScroll(e) {
+// 	console.log("e");
+// }
+
+// const input = document.querySelector(".js-query");
+// input.addEventListener("input", _.debounce(onInput, 300));
+
+// function onInput(e) {
+// 	console.log(e.target.value);
+// }
+// console.log(input);
